bfeffcfcf2d5d5431406ab847186aec0e5e2c66c
Christopher Martinez <indychris239@gmail.com>
2026-01-18 20:52:48 -0500

    web/main.go Change name to layout.html

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
-	tmpl, err := template.ParseFiles("./web/templates/main.html")
+	tmpl, err := template.ParseFiles("./web/templates/layout.html")
 	if err != nil {
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 		return
