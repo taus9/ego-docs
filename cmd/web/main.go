dacfbd3a0a05397eaa59ee0bf2a6e533d308c698
Christopher Martinez <indychris239@gmail.com>
2026-01-18 20:51:36 -0500

    Basic home server

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,5 +1,28 @@
 package main
 
+import (
+	"html/template"
+	"log"
+	"net/http"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	tmpl, err := template.ParseFiles("./web/templates/main.html")
+	if err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+	err = tmpl.Execute(w, nil)
+	if err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+	}
+}
+
 func main() {
-	// Application entry point
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+
+	log.Println("Starting server on :4000")
+	err := http.ListenAndServe(":4000", mux)
+	log.Fatal(err)
 }
