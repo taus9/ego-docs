ac38a45f34580a719bd6be1347a269a4452bdb85
Christopher Martinez <indychris239@gmail.com>
2026-01-19 19:18:39 -0500

    Add link hightlighting

--- web/static/nav.js
+++ web/static/nav.js
@@ -1,22 +1,45 @@
-      const sidebar = document.getElementById("sidebar");
-      const overlay = document.getElementById("overlay");
-      const openBtn = document.getElementById("openSidebar");
-      const closeBtn = document.getElementById("closeSidebar");
+const sidebar = document.getElementById("sidebar");
+const overlay = document.getElementById("overlay");
+const openBtn = document.getElementById("openSidebar");
+const closeBtn = document.getElementById("closeSidebar");
+let lastTarget = document.getElementById("start-link");
 
-      function openSidebar() {
-        sidebar.classList.remove("-translate-x-full");
-        overlay.classList.remove("hidden");
-      }
+const focusedClassName = "flex items-center gap-3 rounded-lg px-3 py-2 bg-gray-800 text-white font-medium cursor-pointer"
+const unfocusedClassName = "flex items-center gap-3 rounded-lg px-3 py-2 text-gray-300 hover:bg-gray-800 hover:text-white cursor-pointer"
 
-      function closeSidebar() {
-        sidebar.classList.add("-translate-x-full");
-        overlay.classList.add("hidden");
-      }
+const focusedSpanClassName = "h-2 w-2 rounded-full bg-purple-400"
+const unfocusedSpanClassName =  "h-2 w-2 rounded-full bg-gray-500"
 
-      openBtn?.addEventListener("click", openSidebar);
-      closeBtn?.addEventListener("click", closeSidebar);
-      overlay?.addEventListener("click", closeSidebar);
+function openSidebar() {
+  sidebar.classList.remove("-translate-x-full");
+  overlay.classList.remove("hidden");
+}
 
-      window.addEventListener("resize", () => {
-        if (window.innerWidth >= 768) closeSidebar();
-      });
+function closeSidebar() {
+  sidebar.classList.add("-translate-x-full");
+  overlay.classList.add("hidden");
+}
+
+openBtn?.addEventListener("click", openSidebar);
+closeBtn?.addEventListener("click", closeSidebar);
+overlay?.addEventListener("click", closeSidebar);
+
+window.addEventListener("resize", () => {
+  if (window.innerWidth >= 768) closeSidebar();
+});
+
+sidebar.addEventListener("click", (event) => {
+  const topicLink = event.target.closest("a");
+  if (!topicLink) return;
+
+  lastTarget?.setAttribute("class", unfocusedClassName);
+  topicLink.setAttribute("class", focusedClassName);
+
+  const lastSpan = lastTarget?.querySelector("span");
+  if (lastSpan) lastSpan.setAttribute("class", unfocusedSpanClassName);
+
+  const currentSpan = topicLink.querySelector("span");
+  if (currentSpan) currentSpan.setAttribute("class", focusedSpanClassName);
+  
+  lastTarget = topicLink;
+});
